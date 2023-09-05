802a0093bae5a583afab95cebe9de09c7229a52d
Henrique Strada <henriquestradax@gmail.com>
2023-09-05 19:49:00 -0300

    Initial commit

---
+++ Desafios em JavaScript/calculadora idade javascript/calcIdade.js
@@ -0,0 +1,44 @@
+document.getElementById("calc").onclick = function() {
+    var nome = prompt("Qual o seu nome?");
+    var dia = parseInt(prompt("Qual dia você nasceu?"));
+    var mes = parseInt(prompt("Qual mês você nasceu?"));
+    var ano = parseInt(prompt("Qual ano você nasceu?"));
+    var dataAtual = new Date();
+    var anoAtual = dataAtual.getFullYear();
+    var mesAtual = dataAtual.getMonth() + 1;
+    var diaAtual = dataAtual.getDate();
+    var anos = anoAtual - ano;
+    var meses = mesAtual - mes;
+    var dias = diaAtual - dia;
+    if (meses < 0 || (meses == 0 && dias < 0)) {
+        anos--;
+        meses += 12;
+    }
+
+    if (dias < 0) {
+        meses--;
+        dias += new Date(anoAtual, mesAtual - 1, 0).getDate();
+    }
+    alert(nome + ", sua idade é " + anos + " anos, " + meses + " meses e " + dias + " dias.");
+    alert("Daqui 30 anos, você tera: " + (anos + 30) + " anos, e em dias tera: " + (anos + 30 * 365) + " dias de vida.");
+
+    if(anos >= 16){
+        alert(nome + ", na sua idade atual, você está apto para votar!")
+    }else{
+        alert(nome + ", na sua idade atual, você não está apto para votar!")
+    }
+
+    meses = meses + 40;
+    if (meses >= 12) {
+        anos += Math.floor(meses / 12);
+        meses = meses % 12;
+    }
+    if (anos >= 18) {
+        alert(nome + ", daqui 40 meses você será maior de idade.");
+        alert(nome + ", sua idade será de " + anos + " anos, " + meses + " meses e " + dias + " dias.");
+    } else {
+        alert(nome + ", daqui 40 meses você ainda não será maior de idade.");
+        alert(nome + ", sua idade será de " + anos + " anos, " + meses + " meses e " + dias + " dias.");
+    }
+
+}
